d946604d3a688be072f4d19c6583f9c2d0eeadaa
Arturo Quintanilla <art9518@gmail.com>
2022-02-25 18:40:27 +0000

    Zipped projects

--- solidity_examples/AwesomeContract.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity >=0.7.4;
-
-contract AwesomeContract {
-    string public value = "its a secret";
-    
-    function set(string memory _value) public {
-        value = _value;
-    }
-}
